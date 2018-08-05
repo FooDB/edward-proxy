af7c35e150fcba7206c0834d576c34d410bb2c48
Michael Lee <lee.cs.michael@gmail.com>
2018-08-04 17:52:32 -0700

    Finalize proxy-server

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const port = process.env.PORT || 55055;
+
+app.use(express.static(path.join(__dirname, './public')));
+app.use('/restaurant/:id/', express.static('./public'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -8,11 +13,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET');
   next();
 });
-
-app.use('/', express.static('./public'))
-app.use('/restaurant/:id', express.static('./public/'));
-
-
 
 app.listen(port, () => {
   console.log(`server running at: http://127.0.0.1:${port}`);
