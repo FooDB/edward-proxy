92db6d9fb2d3a5bdee8204b9bead9be1b9db7ca5
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-13 11:02:50 -0700

    Add proxy middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
+const proxy = require('http-proxy-middleware');
 const path = require('path');
-
-const port = process.env.PORT || 55055;
-
-app.use(express.static(path.join(__dirname, './public')));
-app.use('/restaurant/:id/', express.static('./public'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -14,6 +10,25 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, './public')));
+app.use('/restaurant/:id/', express.static('./public'));
+
+// photos proxy
+app.use('/api/restaurant/:id/photos', proxy({ target: 'http://ec2-34-201-243-233.compute-1.amazonaws.com/' }));
+
+// reviews proxy
+app.use('api/restaurant/:id/filterKeywords', proxy({ target:  'http://ec2-34-207-216-56.compute-1.amazonaws.com/' }));
+
+// overview proxy
+app.use('/restaurant/:id/general', proxy({ target: 'http://ec2-18-188-170-47.us-east-2.compute.amazonaws.com/' }));
+
+// menu proxy
+app.use('/restaurant/:id/general', proxy({ target: 'http://ec2-54-172-30-13.compute-1.amazonaws.com/' }));
+app.use('/restaurant/:id/lovedFor', proxy({ target: 'http://ec2-54-172-30-13.compute-1.amazonaws.com/' }));
+app.use('/restaurant/:id/reviews', proxy({ target: 'http://ec2-54-172-30-13.compute-1.amazonaws.com/' }));
+app.use('/restaurant/:id/info', proxy({ target: 'http://ec2-54-172-30-13.compute-1.amazonaws.com/' }));
+
+const port = process.env.PORT || 55055;
 app.listen(port, () => {
   console.log(`server running at: http://127.0.0.1:${port}`);
 });
